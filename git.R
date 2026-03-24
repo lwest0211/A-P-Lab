1013791391af8126d36839a304375f4da98b02f3
lwest0211 <we1234195@email.ccbcmd.edu>
2026-03-24 18:39:49 +0000

    add template files

--- git.R
+++ git.R
@@ -1,9 +1,6 @@
-
 usethis::use_git_config(
-  user.name = "lwest0211",
-  user.email = "we1234195@email.ccbcmd.edu",
+  user.name = "maptv",
+  user.email = "mlaptev@ccbcmd.edu",
 )
 
 usethis::create_github_token()
-
-gitcreds::gitcreds_set()
